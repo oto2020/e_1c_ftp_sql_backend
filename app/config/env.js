606eeeed0b0181026be27e13d02dbb546edcef81
igo4ek <babka2002@ya.ru>
2023-11-27 11:43:32 +0300

    Стартовая конфигурация

---
+++ app/config/env.js
@@ -0,0 +1,9 @@
+const env = {
+    database: "fitness1c",
+    username: "root",
+    password: "root",
+    host: "localhost",
+    dialect: "mysql",
+  };
+  
+  module.exports = env;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// include database config file
+const db = require("./app/config/db.config.js");
+
+// force: true will drop the table if it already exists
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync with { force: true }");
+});
+
+// include application routes
+require("./app/route/book.route.js")(app);
+
+// Create & Listen Server
+let server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Application request listening at http://%s:%s", host, port);
+});
